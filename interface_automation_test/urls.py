def4209103111bebba579d343e4e6c03814154a8
gaoyinglong <gaoyinglong@kong.net>
2018-06-22 16:01:29 +0800

    add login api

--- interface_automation_test/urls.py
+++ interface_automation_test/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api_test import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',views.index),
+    path('login_action/',views.login_action),
+    path('event_manage/',views.event_manage),
+    path('accounts/login/',views.index),
 ]
